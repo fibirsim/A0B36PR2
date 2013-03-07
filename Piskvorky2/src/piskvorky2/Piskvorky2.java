8abc2622b27609fad4e8e9a031ae57f58cfdca47
Šímek <mek@147.32.222.155>
2013-03-07 13:02:30 +0100

    Začátek. č.2

--- Piskvorky2/src/piskvorky2/Piskvorky2.java
+++ Piskvorky2/src/piskvorky2/Piskvorky2.java
@@ -1,19 +1,11 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package piskvorky2;
 
-/**
- *
- * @author Šímek
- */
+
 public class Piskvorky2 {
 
-    /**
-     * @param args the command line arguments
-     */
+    
     public static void main(String[] args) {
-        // TODO code application logic here
+        
     }
 }
